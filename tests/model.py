53466e5cee05b6975662b86099306ade83102ea1
“walid <wasefiani@gmail.com>
2021-04-05 17:51:51 +0200

    c

--- tests/model.py
+++ tests/model.py
@@ -2,8 +2,19 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 df = pd.read_csv("/Users/famille//projetstat/.aws/df_modelisation.csv",error_bad_lines=False,index_col=0)
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+#suppression des prix>10^5
+indexNames3 = df[ df["prix_vente"]  >10**5].index
+df.drop(indexNames3, inplace=True)
+
+
 #df=df.head(1000)
 y=df["prix_vente"]
+
 
 #correlation
 '''
@@ -18,39 +29,44 @@ print(df[[u"age", u"kilometrage",u"puissance_fiscale",u"horsepower","engine"]].c
 #suppression de var corr et prix de vente
 
 df1=df.drop(["prix_vente","horsepower","modele_com","date_mec","options","departement","date_publication","date_depublication","kilometrage"],axis=1)
-df1["premiere_main"].replace("oui",1,inplace= True)
-df1["premiere_main"].replace("non",0,inplace= True)
-df1=pd.get_dummies(df[["energie","boite_de_vitesse","porte","couleur","premiere_main","Finition"]])
+#df1["premiere_main"].replace("oui",1,inplace= True)
+#df1["premiere_main"].replace("non",0,inplace= True)
+df1['porte'] = df1['porte'].astype(str)
+df1=pd.get_dummies(df1[["energie","boite_de_vitesse","couleur","premiere_main","Finition","porte"]])
 
 
 
 # Variables quantitatives
-df2=df[["engine","puissance_fiscale","age","horsepower",'axe 1', 'axe 2',
+df2=df[["engine","age","horsepower",'axe 1', 'axe 2',
        'axe 3', 'axe 4', 'axe 5', 'axe 6', 'axe 7', 'axe 8', 'axe 9', 'axe 10',
        'axe 11', 'axe 12', 'axe 13']]
+
+
 
 # Concaténation
 X=pd.concat([df1,df2],axis=1)
 
+#Normer les variables explicatives quanti
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+'''
+X[["engine","age","horsepower",'axe 1', 'axe 2',
+       'axe 3', 'axe 4', 'axe 5', 'axe 6', 'axe 7', 'axe 8', 'axe 9', 'axe 10',
+       'axe 11', 'axe 12', 'axe 13']]=scaler.fit_transform(X[["engine","age","horsepower",'axe 1', 'axe 2',
+       'axe 3', 'axe 4', 'axe 5', 'axe 6', 'axe 7', 'axe 8', 'axe 9', 'axe 10',
+       'axe 11', 'axe 12', 'axe 13']])
+'''
 
-#Normer les variables explicatives
-scaler = MinMaxScaler(feature_range=(0, 1))
-X= scaler.fit_transform(X)
+#y=y.to_frame()
+#y=scaler.fit_transform(y)
+#y = pd.DataFrame(y,columns=['prix_vente'])
 
 #spliter les données
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=11)
+import numpy as np
+X_train,X_test,y_train,y_test=train_test_split(X,np.log(y),test_size=0.25,random_state=11)
 X_train = pd.DataFrame(X_train)
 X_test = pd.DataFrame(X_test)
 
-#faut-il traiter aussi la variable target
-'''
-y_train_scaled = scaler.fit_transform(y_train)
-y_test_scaled = scaler.fit_transform(y_test)
-y_train = pd.DataFrame(y_train_scaled)
-y_test = pd.DataFrame(y_test_scaled)
-y_train_scaled = scaler.fit_transform(y_train)
-y_test_scaled = scaler.fit_transform(y_test)
-'''
 
 
 from sklearn.model_selection import GridSearchCV
@@ -60,15 +76,20 @@ knn = KNeighborsRegressor()
 
 #choix de k: nb du plus proche voisin par deux méthodes
 #méthode1:
+'''
 from sklearn.model_selection import validation_curve
 train_score,val_score=validation_curve(knn,X_train,y_train,'n_neighbors',params,cv=5)
+'''
 #méthode2: en utilisant grid search pour le choix de la métrique aussi
 import numpy as np
-param_grid={'n_neighbors': np.arange(6890,6905),'metric':['euclidean','manhattan']}
+#param_grid={'n_neighbors': np.arange(1,100),'metric':['euclidean','manhattan']}
+'''
+param_grid={'n_neighbors': np.arange(6,9)}
 from sklearn.model_selection import GridSearchCV
 knn = GridSearchCV(knn, param_grid, cv=5)
 knn.fit(X_train,y_train)
 print(knn.best_params_)
+'''
 #{'metric': 'manhattan', 'n_neighbors': 6900}
 
 #pourcentage de données utiles à récolter
@@ -86,9 +107,9 @@ plt.legend()
 #entrainer le modèle
 import math
 from sklearn.neighbors import KNeighborsRegressor
-knn = KNeighborsRegressor(metric= 'manhattan', n_neighbors= 6900)
+knn = KNeighborsRegressor(metric= 'manhattan', n_neighbors= 6,weights='distance')# les prix ne sont pas uniformement distribués
 knn.fit(X_train,y_train)
-
+print(knn.score(X_test,y_test))
 #prédiction
 pred=knn.predict(X_test)
 
@@ -97,23 +118,38 @@ import math
 from sklearn.metrics import mean_squared_error
 error = math.sqrt(mean_squared_error(y_test,pred)) #calculate rmse
 score= knn.score(X_test,y_test)
-print('RMSE value for k= ', 6900 , 'is:', error)
-print('score value for k= ', 6900 , 'is:', score)
+
+#Defining MAPE function
+def MAPE(Y_actual,Y_Predicted):
+    mape = np.mean(np.abs((Y_actual - Y_Predicted)/Y_actual))*100
+    return mape
+mape = MAPE(y_test, pred)
+# à voir scicit learn
+
+
+#erreure quadratique moyenne
+print('RMSE value for k= ', 6 , 'is:', error)
+#sans traitement=1578.0976223381065
+#log=0.17273758688079002
+#norm=0.4089707745255511 //c'est normal car on est oentre 0 et 1 et la différence au carré est de plus en plus en petite
+
+#R^2
+print('score value for k= ', 6 , 'is:', score)
+#sans traitement=0.8357542311811924
+#log=0.8262774212423686
+#norm=0.8357542311811924
+
+#erreure en valeur absolue
+print('MAPE value for k= ', 6 , 'is:', mape)
+#sans traitement=12.423715479695412%
+#log=1.3146505898190326%
+#norm=128.825381 %
+'''
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.plot(X_test,pred,c='r')
-
-#algorithme backward:
-def modeleFin(model,X,y):
-       X=X
-       X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=11)
-       X_train = pd.DataFrame(X_train)
-       X_test = pd.DataFrame(X_test)
-       knn = KNeighborsRegressor(metric='manhattan', n_neighbors=6900)
-       knn.fit(X_train, y_train)
-       score = knn.score(X_test, y_test)
-
+'''
 
 #cas de target qualitative
 '''
@@ -132,3 +168,44 @@ title("Matrice de Confusion")
 colorbar()
 show()
 '''
+
+
+from sklearn.feature_selection import SequentialFeatureSelector
+#selection des variables les plus impactantes avec l'algorithme forward
+import numpy as np
+
+#recherche par validation croisée du meilleur paramètre du nombre de variables à sélectionner
+'''
+param_grid={'n_features_to_select': np.arange(2,4)}
+from sklearn.model_selection import GridSearchCV
+sfw = SequentialFeatureSelector(knn, direction='forward')
+sfw = GridSearchCV(sfw, param_grid,scoring=mean_squared_error, cv=5)
+sfw.fit(X_train,y_train)
+print(sfw.best_params_)
+'''
+
+sfw = SequentialFeatureSelector(knn,direction='forward')
+sfw.fit(X_train,y_train)
+print(sfw.get_support())
+print(sfw.transform(X).shape)
+pos=[]
+for i in range(45):
+    if sfw.get_support()[i]==True:
+        pos.append(i)
+colnameB = X.columns[pos]
+
+
+#selection des variables les plus impactantes avec l'algorithme backward
+sbw = SequentialFeatureSelector(knn, n_features_to_select= ,direction='backward')
+sbw.fit(X_train,y_train)
+print(sbw.get_support())
+print(sbw.transform(X).shape)
+pos=[]
+for i in range(45):
+    if sfw.get_support()[i]==True:
+        pos.append(i)
+colnamef = X.columns[pos]
+
+
+
+#définition du cp de mallows
