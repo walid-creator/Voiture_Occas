b7cb775bb653d63c66a8b08711bde7f3b0f7aab1
Corentin NIO <coco.nio@live.fr>
2021-04-14 19:09:27 +0200

    save

--- .aws/regression.py
+++ .aws/regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 
@@ -9,7 +10,7 @@ table = pd.read_csv('D:/Projet stat 2A/projet_stat/.aws/df_modelisation.csv')
 
 y = table["prix_vente"].to_numpy()
 x = table.drop(["prix_vente","date_mec","options","date_publication","date_depublication","departement"], axis = 1)
-x= x.drop(["age","puissance_fiscale"], axis = 1)
+x = x.drop(["age","puissance_fiscale"], axis = 1)
 x_quanti = x.drop(["modele_com","energie","boite_de_vitesse","couleur","premiere_main","Finition"], axis = 1)
 x_quanti = x_quanti.iloc[0:len(x_quanti),1:21]
 x_quanti = x_quanti.to_numpy()
@@ -35,28 +36,25 @@ XTrain = sm.add_constant(XTrain)
 print(XTrain.head())
 
 YTrain = table["prix_vente"]
+YTrain = np.log(YTrain)
 
-reg = sm.OLS(YTrain,XTrain)
+xTrain, xTest, yTrain, yTest = train_test_split(XTrain, YTrain, test_size = 0.3, random_state = 5)
+
+reg = sm.OLS(yTrain,xTrain)
 resReg = reg.fit()
 
 print(resReg.summary())
 
-### On enleve l'axe 1
+yPred = resReg.predict(xTest)
 
-XTrain = XTrain.drop(["axe 1","axe 4"], axis = 1)
+mape_reg = abs((np.exp(yTest) - np.exp(yPred))/np.exp(yTest))
+n = len(mape_reg)
+MAPE_reg = (mape_reg.sum())/n
 
-reg = sm.OLS(YTrain,XTrain)
-resReg = reg.fit()
+ecart_type_mape = (mape_reg-MAPE_reg)**2
+ecart_type_mape = np.sqrt(ecart_type_mape.sum()/n)
 
-print(resReg.summary())
-
-
-model = LinearRegression()
-model.fit(XTrain,YTrain)
-
-r_sq = model.score(XTrain,YTrain)
-print('coefficient of determination:', r_sq)
-
+print(MAPE_reg,ecart_type_mape)
 
 
 
